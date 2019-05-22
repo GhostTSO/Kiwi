bf163fbf5543a389b3d27f9f1f4b1a09df4a09b4
Ghost <Ghost@192.168.1.198>
2019-05-22 00:02:49 -0600

    More effects to Inverted Circularity

--- Kiwi/src/kiwi/core/effect/InvertedCircularity.java
+++ Kiwi/src/kiwi/core/effect/InvertedCircularity.java
@@ -15,6 +15,13 @@ public class InvertedCircularity extends Effect{
 	
 	double[] peaksLeft = new double[Source.SAMPLES/4];
 	double[] peaksRight = new double[Source.SAMPLES/4];
+	int degree = 0;
+	int startingColor1 = 200;
+	int startingColor2 = 100;
+	int startingColor3 = 25;
+	int colorIncrementer1 = 1;
+	int colorIncrementer2 = 2;
+	int colorIncrementer3 = 3;
 	public InvertedCircularity() {
 		super("Inverted Circularity");
 	}
@@ -23,8 +30,8 @@ public class InvertedCircularity extends Effect{
 	public void render(RenderContext context) {
 		
 		context.g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		context.g2D.setColor(Color.BLACK);
+		Color myColor = new Color(startingColor1/2, startingColor2/2, startingColor3/2);
+		context.g2D.setColor(myColor);
 		context.g2D.setStroke(new BasicStroke((context.canvas_w/Source.SAMPLES)+8));
 		context.g2D.fillRect(
 				0,
@@ -38,17 +45,19 @@ public class InvertedCircularity extends Effect{
 		double l_normal = Math.pow(context.stereo_l[Source.SAMPLES/2].re, 1/2.75);
 		double r_normal = Math.pow(context.stereo_r[Source.SAMPLES/2].re, 1/2.75);
 		
-		double heightMultiplier = (double)context.canvas_h/25;
-		Color myColor;
+		double heightMultiplier = (double)context.canvas_h/12.5;
 		
-		int circleWidth = context.canvas_h/50;
+		
+		int circleWidth = context.canvas_h/35;
 		
 		float degree;
 		double cosValue;
 		double sinValue;
 		
+		
+		
 		for(int i = 0; i < Source.SAMPLES/4; i ++) {
-			myColor= new Color(i,0, 255);
+			myColor= new Color(startingColor1, startingColor2, startingColor3);
 			l_root = Math.pow(context.stereo_l[i+3*Source.SAMPLES/4].re, 1/2.75);
 			r_root = Math.pow(context.stereo_r[i+1].re, 1/2.75);
 			context.g2D.setColor(myColor);	
@@ -57,17 +66,17 @@ public class InvertedCircularity extends Effect{
 				peaksLeft[i] = (heightMultiplier*(l_root-l_normal));
 			}
 			else if(peaksLeft[i] > 0){
-				peaksLeft[i]-=5;
+				peaksLeft[i]-=4;
 			}
 			
 			if(heightMultiplier*(r_root-r_normal) > peaksRight[i]) {
 				peaksRight[i] = (heightMultiplier*(r_root-r_normal));
 			}
 			else if(peaksRight[i] > 0){
-				peaksRight[i]-=5;
+				peaksRight[i]-=4;
 			}
 			
-			degree = (float)(i*Math.PI/256.0);
+			degree = (float)((this.degree+i*Math.PI)/256.0);
 			
 				
 			
@@ -81,8 +90,7 @@ public class InvertedCircularity extends Effect{
 					circleWidth
 					);	
 			
-			myColor= new Color(255-i,0, 255);
-			context.g2D.setColor(myColor);
+			
 			
 			cosValue = -context.canvas_w/4*Math.cos(degree)+ peaksRight[i]*Math.cos(degree);
 			sinValue = -context.canvas_w/4*Math.sin(degree)+ peaksRight[i]*Math.sin(degree);
@@ -94,6 +102,23 @@ public class InvertedCircularity extends Effect{
 					circleWidth
 					);	
 			
+		}
+		
+		this.startingColor1 += this.colorIncrementer1;
+		this.startingColor2 += this.colorIncrementer2;
+		this.startingColor3 += this.colorIncrementer3;
+		if(this.startingColor1 > 254 || this.startingColor1 < 5) {
+			this.colorIncrementer1*=-1;
+		}
+		if(this.startingColor2 > 253 || this.startingColor2 < 5) {
+			this.colorIncrementer2*=-1;
+		}
+		if(this.startingColor3 > 252 || this.startingColor3 < 5) {
+			this.colorIncrementer3*=-1;
+		}
+		this.degree++;
+		if(this.degree > 359) {
+			degree = 0;
 		}
 	}
 
