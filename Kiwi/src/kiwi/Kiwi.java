d61d4412d7bb5d193bbcf7fae24e8366a7106e00
smcge <smcge@192.168.0.2>
2019-06-21 21:49:44 -0600

    1.1.2
    Reverted previous change

--- Kiwi/src/kiwi/Kiwi.java
+++ Kiwi/src/kiwi/Kiwi.java
@@ -5,7 +5,7 @@ import kiwi.core.Engine;
 public class Kiwi {
 	
 	public static final Version
-		VERSION = new Version("Kiwi", 1, 1, 1);
+		VERSION = new Version("Kiwi", 1, 1, 2);
 	
 	public static void main(String[] args) {
 		Engine engine = new Engine();
