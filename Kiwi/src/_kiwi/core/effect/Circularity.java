cb4b89a6168fa90e43fa00517d9d3193d8e00bcc
Ghost <Ghost@192.168.1.134>
2019-08-29 17:26:42 -0600

    Updated Circularity effect to be filled in

--- Kiwi/src/_kiwi/core/effect/Circularity.java
+++ Kiwi/src/_kiwi/core/effect/Circularity.java
@@ -1,8 +1,9 @@
 package _kiwi.core.effect;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.RenderingHints;
+import java.awt.geom.GeneralPath;
 
 import _kiwi.core.source.Source;
 
@@ -24,6 +25,9 @@ public class Circularity extends Effect {
 	double botXValue; //y pos value for bottom circle point
 	double botYValue; //y pos value for bottom circle point
 
+	//points storage to draw the shape
+	double points[][] = new double[2][510];
+
 	//diameter of the circle
 	int circleWidth;
 
@@ -36,6 +40,17 @@ public class Circularity extends Effect {
 	int lastBotX;
 	int lastBotY;
 
+	//variable to store color state
+	double colorCounter1 = 120;
+	double colorCounter2 = 60;
+	double colorCounter3 = 20;
+
+	//Variable to flip to control color speed
+	double colorSpeed1 = .5;
+	double colorSpeed2 = .2;
+	double colorSpeed3 = .3;
+
+
 
 	//contstructor
 	public Circularity() {
@@ -46,31 +61,20 @@ public class Circularity extends Effect {
 	@Override
 	public void onRender(RenderContext context) {
 
+		//create a render path to make the shape
+		GeneralPath circle = new GeneralPath();
+
+		//set speed of movement based on canvas size
 		speed = context.canvas_h/100;
 
 		//toggle antialising
 		context.g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		//fill background to black
-		context.g2D.setColor(Color.BLACK);
-		context.g2D.setStroke(new BasicStroke((int) speed));
-		context.g2D.fillRect(
-				0,
-				0,
-				context.canvas_w,
-				context.canvas_h
-				);			
-
 		//set scale value based on height
 		scale = context.canvas_h/divider;
 
-		//color value
-		Color myColor;
-
 		//setting the circle width
 		circleWidth = context.canvas_h/50;
-
-
 
 		//for loop to go through 1/4 of the samples
 		for(int i = 0; i < Source.SAMPLES/4; i ++) {
@@ -100,7 +104,7 @@ public class Circularity extends Effect {
 				}
 
 			//calculate current degrees
-			degree = (float)(i*Math.PI/256.0);
+			degree = (float)(i*Math.PI/255.0);
 
 			//calculate x and y position for the top arc
 			topXValue = (context.canvas_w/8*Math.cos(degree)+ peaksLeft[i]*Math.cos(degree))+context.canvas_w/2;
@@ -131,60 +135,73 @@ public class Circularity extends Effect {
 				}
 
 
-			//set the color to make gradient effect
-			myColor= new Color(255-i,i, 255);
-			context.g2D.setColor(myColor);
-
 			//calculate x and y position for the top arc
 			botXValue = (context.canvas_w/8*Math.cos(-degree)+ peaksRight[i]*Math.cos(-degree))+context.canvas_w/2;
 			botYValue = (context.canvas_w/8*Math.sin(-degree)+ peaksRight[i]*Math.sin(-degree))+context.canvas_h/2;
 
-			//as long as its not the first of last value draw line this way
-			if(i != 0 && i != Source.SAMPLES/4-1) {
+			//store the points into the point array to be rendered later
+			points[0][i] = (int)topXValue;
+			points[1][i] = (int)topYValue;
+			points[0][509-i] = (int)botXValue;
+			points[1][509-i] = (int)botYValue;
+		}						
 
-				//draw line from current top point to current last
-				context.g2D.drawLine(	lastTopX, 
-						lastTopY, 
-						(int)topXValue, 
-						(int)topYValue);
+		//oscillate color 1 speed
+		if(colorCounter1 > 250) {
+			colorSpeed1 *= -1;
+		}else if(colorCounter1 < 20) {
+			colorSpeed1 *= -1;
+			colorCounter1 = 20;
+		}
 
-				//draw line from current bottom point to last bottom point
-				context.g2D.drawLine(	lastBotX, 
-						lastBotY, 
-						(int)botXValue, 
-						(int)botYValue);
+		//oscillate color 2 speed
+		if(colorCounter2 > 250) {
+			colorSpeed2 *= -1;
+		}else if(colorCounter2 < 20) {
+			colorSpeed2 *= -1;
+			colorCounter2 = 20;
+		}
 
-			}
-			else //first value draw this way
-				if(i == 0){
-					context.g2D.drawLine((int)topXValue, (int)topYValue, (int)botXValue, (int)botYValue);
-				}else //last value needs to be drawn this way
-				{
-					//connect the last top and bottom points
-					context.g2D.drawLine((int)topXValue, (int)topYValue, (int)botXValue, (int)botYValue);
+		//oscillate color 3 speed
+		if(colorCounter3 > 250) {
+			colorSpeed3 *= -1;
+		}else if(colorCounter3 < 20) {
+			colorSpeed3 *= -1;
+			colorCounter3 = 20;
+		}
 
-					//draw line from current top point to current last
-					context.g2D.drawLine(	lastBotX, 
-							lastBotY, 
-							(int)botXValue, 
-							(int)botYValue);
+		//increment colors
+		colorCounter1 +=colorSpeed1;
+		colorCounter2 += colorSpeed2;
+		colorCounter3 += colorSpeed3;
 
-					//draw line from current bottom point to current last
-					context.g2D.drawLine(	lastTopX, 
-							lastTopY, 
-							(int)topXValue, 
-							(int)topYValue);
-				}
+		//create our colors
+		Color color1 = new Color((int)colorCounter3, (int)(255-colorCounter2), (int)colorCounter1);
+		Color color2 = new Color((int)colorCounter2,(int)(colorCounter3), (int)255);
+		Color color3 = new Color(255,(int)(255-colorCounter1), (int)colorCounter3);
+		Color color4 = new Color((int)(255-colorCounter1),255, (int)(255-colorCounter3));
+		
+		
+		//color gradient for the background
+		GradientPaint gp4 = new GradientPaint(0, 0, 
+				color4, context.canvas_w/12, context.canvas_h, color3, true);
+		context.g2D.setPaint(gp4);
+		context.g2D.fillRect(0, 0, context.canvas_w, context.canvas_h);
+		
+		//color gradient for circle
+		gp4 = new GradientPaint(0, 0, 
+				color1, context.canvas_w/12, context.canvas_h, color2, true);
+		context.g2D.setPaint(gp4);
+		
+		//begin drawing the shape
+		circle.moveTo(points[0][0], points[1][0]);		
 
-			//set last values to equal current for next cycle in for loop
-			lastTopX = (int) topXValue;
-			lastTopY = (int) topYValue;
-			lastBotY = (int) botYValue;
-			lastBotX = (int) botXValue;
+		//draw the rest of the points
+		for (int k = 1; k < 509; k++) 
+			circle.lineTo(points[0][k], points[1][k]);
 
-		}			
-
-
-
+		//finish the shape
+		circle.closePath();
+		context.g2D.fill(circle);
 	}
 }
