3a37c0323b4087ae26f2803b4777daeb2a662a55
Ghost <Ghost@Ghost.uccs.edu>
2019-09-16 20:42:15 -0600

    Updated Circularities design

--- Kiwi/src/_kiwi/core/effect/effects/Circularity.java
+++ Kiwi/src/_kiwi/core/effect/effects/Circularity.java
@@ -1,9 +1,14 @@
 package _kiwi.core.effect.effects;
 
-import java.awt.Color;
-import java.awt.GradientPaint;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.TexturePaint;
 import java.awt.geom.GeneralPath;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import _kiwi.core.effect.Effect;
 import _kiwi.core.source.Source;
@@ -39,55 +44,83 @@ public class Circularity extends Effect {
 	int lastBotY;
 
 	//variable to store color state
-	double colorCounter1 = 120;
-	double colorCounter2 = 60;
-	double colorCounter3 = 20;
+//	double colorCounter1 = 120;
+//	double colorCounter2 = 60;
+//	double colorCounter3 = 20;
 
 	//Variable to flip to control color speed
-	double colorSpeed1;
-	double colorSpeed2;
-	double colorSpeed3;
+//	double colorSpeed1;
+//	double colorSpeed2;
+//	double colorSpeed3;
 
-
+	BufferedImage backgroundImage; 
+	BufferedImage foregroundImage; 
+	TexturePaint backPaint;
+	TexturePaint forePaint;
+	
 
 	//contstructor
 	public Circularity() {
 		super("Circularity");
-		colorSpeed1 = Math.random()*.5;
-		colorSpeed2 = Math.random()*.8;
-		colorSpeed3 = Math.random()*.25;
+//		colorSpeed1 = Math.random()*.5;
+//		colorSpeed2 = Math.random()*.8;
+//		colorSpeed3 = Math.random()*.25;
+//		
+//		colorCounter1 = Math.random() * 200;
+//		colorCounter2 = Math.random() * 200;
+//		colorCounter3 = Math.random() * 200;
 		
-		colorCounter1 = Math.random() * 200;
-		colorCounter2 = Math.random() * 200;
-		colorCounter3 = Math.random() * 200;
+		
+		//load background image
+				try {
+					backgroundImage = ImageIO.read(new File(".\\bin\\_kiwi\\core\\effect\\effects\\resources\\CircularityBackground.png"));
+					foregroundImage = ImageIO.read(new File(".\\bin\\_kiwi\\core\\effect\\effects\\resources\\CircularityForeground.png"));
+					
+				}catch(IOException ex) {
+					System.out.println("Couldn't find file");
+				}
+		
+				
 	}
 
 	//operations needed to render the effect visually
 	@Override
 	public void onRender(RenderContext context) {
+		
+		
+	
+		
 
 		//create a render path to make the shape
 		GeneralPath circle = new GeneralPath();
+		
+		backPaint = new TexturePaint(backgroundImage, new Rectangle(0,0,context.canvas_w,context.canvas_h));
+		
+		context.g2D.drawImage(foregroundImage, 0,0,context.canvas_w,context.canvas_h, 0,0, backgroundImage.getWidth(), backgroundImage.getHeight(), null);
+
 
 		//toggle antialising
 		context.g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		//create our colors
-		Color color1 = new Color((int)colorCounter3, (int)(255-colorCounter2), (int)colorCounter1);
-		Color color2 = new Color((int)colorCounter2,(int)(colorCounter3), (int)255);
-		Color color3 = new Color(255,(int)(255-colorCounter1), (int)colorCounter3);
-		Color color4 = new Color((int)(255-colorCounter1),255, (int)(255-colorCounter3));
+//		Color color1 = new Color((int)colorCounter3, (int)(255-colorCounter2), (int)colorCounter1);
+//		Color color2 = new Color((int)colorCounter2,(int)(colorCounter3), (int)255);
+//		Color color3 = new Color(255,(int)(255-colorCounter1), (int)colorCounter3);
+//		Color color4 = new Color((int)(255-colorCounter1),255, (int)(255-colorCounter3));
 		
 		//color gradient for the background
-		GradientPaint gp4 = new GradientPaint(0, 0, 
-				color4, context.canvas_w, context.canvas_h, color3, true);
-		context.g2D.setPaint(gp4);
-		context.g2D.fillRect(0, 0, context.canvas_w, context.canvas_h);
+		//GradientPaint gp4 = new GradientPaint(0, 0, 
+		//		color4, context.canvas_w, context.canvas_h, color3, true);
+		//context.g2D.setPaint(gp4);
+		//context.g2D.setColor(new Color(0,0,0));
+		//context.g2D.fillRect(0, 0, context.canvas_w, context.canvas_h);
+		
+		context.g2D.setPaint(backPaint);
 		
 		//color gradient for circle
-		gp4 = new GradientPaint(0, 0, 
-				color1, context.canvas_w/5, context.canvas_h, color2, true);
-		context.g2D.setPaint(gp4);
+		//gp4 = new GradientPaint(0, 0, 
+		//		color1, context.canvas_w/5, context.canvas_h, color2, true);
+		//context.g2D.setPaint(gp4);
 		
 		//begin drawing the shape
 		circle.moveTo(points[0][0], points[1][0]);		
@@ -140,8 +173,8 @@ public class Circularity extends Effect {
 					degree = (float)(i*Math.PI/255.0);
 
 					//calculate x and y position for the top arc
-					topXValue = (context.canvas_w/8*Math.cos(degree)+ peaksLeft[i]*Math.cos(degree))+context.canvas_w/2;
-					topYValue = (context.canvas_w/8*Math.sin(degree)+ peaksLeft[i]*Math.sin(degree))+context.canvas_h/2;
+					topXValue = (context.canvas_w/6*Math.cos(degree)+ peaksLeft[i]*Math.cos(degree))+context.canvas_w/2;
+					topYValue = (context.canvas_w/6*Math.sin(degree)+ peaksLeft[i]*Math.sin(degree))+context.canvas_h/2;
 
 					//if the value is worth checking
 					if(context.stereo_r[i+3*Source.SAMPLES/4] > 1) {
@@ -169,8 +202,8 @@ public class Circularity extends Effect {
 
 
 					//calculate x and y position for the top arc
-					botXValue = (context.canvas_w/8*Math.cos(-degree)+ peaksRight[i]*Math.cos(-degree))+context.canvas_w/2;
-					botYValue = (context.canvas_w/8*Math.sin(-degree)+ peaksRight[i]*Math.sin(-degree))+context.canvas_h/2;
+					botXValue = (context.canvas_w/6*Math.cos(-degree)+ peaksRight[i]*Math.cos(-degree))+context.canvas_w/2;
+					botYValue = (context.canvas_w/6*Math.sin(-degree)+ peaksRight[i]*Math.sin(-degree))+context.canvas_h/2;
 
 					//store the points into the point array to be rendered later
 					points[0][i] = (int)topXValue;
@@ -179,33 +212,33 @@ public class Circularity extends Effect {
 					points[1][509-i] = (int)botYValue;
 				}						
 
-				//oscillate color 1 speed
-				if(colorCounter1 > 250) {
-					colorSpeed1 *= -1;
-				}else if(colorCounter1 < 20) {
-					colorSpeed1 *= -1;
-					colorCounter1 = 20;
-				}
-
-				//oscillate color 2 speed
-				if(colorCounter2 > 250) {
-					colorSpeed2 *= -1;
-				}else if(colorCounter2 < 20) {
-					colorSpeed2 *= -1;
-					colorCounter2 = 20;
-				}
-
-				//oscillate color 3 speed
-				if(colorCounter3 > 250) {
-					colorSpeed3 *= -1;
-				}else if(colorCounter3 < 20) {
-					colorSpeed3 *= -1;
-					colorCounter3 = 20;
-				}
-
-				//increment colors
-				colorCounter1 +=colorSpeed1;
-				colorCounter2 += colorSpeed2;
-				colorCounter3 += colorSpeed3;
+//				//oscillate color 1 speed
+//				if(colorCounter1 > 250) {
+//					colorSpeed1 *= -1;
+//				}else if(colorCounter1 < 20) {
+//					colorSpeed1 *= -1;
+//					colorCounter1 = 20;
+//				}
+//
+//				//oscillate color 2 speed
+//				if(colorCounter2 > 250) {
+//					colorSpeed2 *= -1;
+//				}else if(colorCounter2 < 20) {
+//					colorSpeed2 *= -1;
+//					colorCounter2 = 20;
+//				}
+//
+//				//oscillate color 3 speed
+//				if(colorCounter3 > 250) {
+//					colorSpeed3 *= -1;
+//				}else if(colorCounter3 < 20) {
+//					colorSpeed3 *= -1;
+//					colorCounter3 = 20;
+//				}
+//
+//				//increment colors
+//				colorCounter1 +=colorSpeed1;
+//				colorCounter2 += colorSpeed2;
+//				colorCounter3 += colorSpeed3;
 	}
 }
