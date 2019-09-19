4f7922515133d7700979a536ed4dbaf70d51a04b
Ghost <Ghost@Ghost.uccs.edu>
2019-09-18 18:49:24 -0600

    Updated Circularity Design and performance

--- Kiwi/src/_kiwi/core/effect/effects/Circularity.java
+++ Kiwi/src/_kiwi/core/effect/effects/Circularity.java
@@ -1,5 +1,6 @@
 package _kiwi.core.effect.effects;
 
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.TexturePaint;
@@ -43,43 +44,23 @@ public class Circularity extends Effect {
 	int lastBotX;
 	int lastBotY;
 
-	//variable to store color state
-//	double colorCounter1 = 120;
-//	double colorCounter2 = 60;
-//	double colorCounter3 = 20;
-
-	//Variable to flip to control color speed
-//	double colorSpeed1;
-//	double colorSpeed2;
-//	double colorSpeed3;
 
 	BufferedImage backgroundImage; 
 	BufferedImage foregroundImage; 
 	TexturePaint backPaint;
-	TexturePaint forePaint;
 	
 
 	//contstructor
 	public Circularity() {
 		super("Circularity");
-//		colorSpeed1 = Math.random()*.5;
-//		colorSpeed2 = Math.random()*.8;
-//		colorSpeed3 = Math.random()*.25;
-//		
-//		colorCounter1 = Math.random() * 200;
-//		colorCounter2 = Math.random() * 200;
-//		colorCounter3 = Math.random() * 200;
-		
 		
 		//load background image
 				try {
 					backgroundImage = ImageIO.read(new File(".\\bin\\_kiwi\\core\\effect\\effects\\resources\\CircularityBackground.png"));
-					foregroundImage = ImageIO.read(new File(".\\bin\\_kiwi\\core\\effect\\effects\\resources\\CircularityForeground.png"));
 					
 				}catch(IOException ex) {
 					System.out.println("Couldn't find file");
 				}
-		
 				
 	}
 
@@ -87,37 +68,15 @@ public class Circularity extends Effect {
 	@Override
 	public void onRender(RenderContext context) {
 		
-		
 		//create a render path to make the shape
 		GeneralPath circle = new GeneralPath();
-		
-		backPaint = new TexturePaint(foregroundImage, new Rectangle(0,0,context.canvas_w,context.canvas_h));
 		
 		context.g2D.drawImage(backgroundImage, 0,0,context.canvas_w,context.canvas_h, 0,0, backgroundImage.getWidth(), backgroundImage.getHeight(), null);
 
 
 		//toggle antialising
 		context.g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-		//create our colors
-//		Color color1 = new Color((int)colorCounter3, (int)(255-colorCounter2), (int)colorCounter1);
-//		Color color2 = new Color((int)colorCounter2,(int)(colorCounter3), (int)255);
-//		Color color3 = new Color(255,(int)(255-colorCounter1), (int)colorCounter3);
-//		Color color4 = new Color((int)(255-colorCounter1),255, (int)(255-colorCounter3));
-		
-		//color gradient for the background
-		//GradientPaint gp4 = new GradientPaint(0, 0, 
-		//		color4, context.canvas_w, context.canvas_h, color3, true);
-		//context.g2D.setPaint(gp4);
-		//context.g2D.setColor(new Color(0,0,0));
-		//context.g2D.fillRect(0, 0, context.canvas_w, context.canvas_h);
-		
-		context.g2D.setPaint(backPaint);
-		
-		//color gradient for circle
-		//gp4 = new GradientPaint(0, 0, 
-		//		color1, context.canvas_w/5, context.canvas_h, color2, true);
-		//context.g2D.setPaint(gp4);
+		context.g2D.setColor(new Color(1f,.0f,.65f,.5f));
 		
 		//begin drawing the shape
 		circle.moveTo(points[0][0], points[1][0]);	
@@ -139,7 +98,6 @@ public class Circularity extends Effect {
 
 		//set speed of movement based on canvas size
 		speed = (60.0*context.dt)* (double) ( context.canvas_h/100.0);
-		System.out.println(context.dt);
 		
 		//set scale value based on height
 				scale = context.canvas_h/divider;
@@ -214,33 +172,5 @@ public class Circularity extends Effect {
 					points[1][509-i] = (int)botYValue;
 				}						
 
-//				//oscillate color 1 speed
-//				if(colorCounter1 > 250) {
-//					colorSpeed1 *= -1;
-//				}else if(colorCounter1 < 20) {
-//					colorSpeed1 *= -1;
-//					colorCounter1 = 20;
-//				}
-//
-//				//oscillate color 2 speed
-//				if(colorCounter2 > 250) {
-//					colorSpeed2 *= -1;
-//				}else if(colorCounter2 < 20) {
-//					colorSpeed2 *= -1;
-//					colorCounter2 = 20;
-//				}
-//
-//				//oscillate color 3 speed
-//				if(colorCounter3 > 250) {
-//					colorSpeed3 *= -1;
-//				}else if(colorCounter3 < 20) {
-//					colorSpeed3 *= -1;
-//					colorCounter3 = 20;
-//				}
-//
-//				//increment colors
-//				colorCounter1 +=colorSpeed1;
-//				colorCounter2 += colorSpeed2;
-//				colorCounter3 += colorSpeed3;
 	}
 }
