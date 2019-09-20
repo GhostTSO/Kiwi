ae221d7bab9d69d885e541cc72d0398c6fc6de12
McGeek07 <s.mcgeek07@gmail.com>
2019-09-20 10:38:55 -0600

    Minor changes

--- Kiwi/src/_kiwi/Kiwi.java
+++ Kiwi/src/_kiwi/Kiwi.java
@@ -9,7 +9,16 @@ public class Kiwi {
 		VERSION = new Version("Kiwi", 2, 1, 0);
 	
 	public static void main(String[] args) {
+		//set the minimum logging priority at any time
+		Debug.setLevel(Debug.DEBUG);
+		
+		//indirect call to the default logger
+		Debug.log(Debug.DEBUG, VERSION);
+		
+		//direct call to the default logger
 		Debug.out.log(VERSION);
+		
+		
 		Engine.init();
 	}
 }
