94e5b6c77048502b5075e6bf4db6899a9665c717
Ghost <Ghost@128.198.204.144>
2019-09-23 15:38:30 -0600

    Updated oscilloscope a bit

--- Kiwi/src/_kiwi/core/effect/effects/Oscilloscope.java
+++ Kiwi/src/_kiwi/core/effect/effects/Oscilloscope.java
@@ -1,14 +1,18 @@
 package _kiwi.core.effect.effects;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 
 import _kiwi.core.effect.Effect;
+import kiwi.core.source.Source;
 
 public class Oscilloscope extends Effect {
 
+	int peak = 50;
 	int[] pointsLeft = new int[600];
 	int[] pointsRight = new int[600];
 	int counter = 0;
+	int repeat = 0;
 	
 	public Oscilloscope() {
 		super("Oscilloscope");
@@ -23,30 +27,39 @@ public class Oscilloscope extends Effect {
 		Color myColor = new Color(0,0,0,.1f);
 		context.g2D.setColor(myColor);
 		context.g2D.fillRect(0, 0, context.canvas_w, context.canvas_h);
-		for(int i = 0; i < pointsLeft.length-1; i++) {
+		myColor = new Color(255,0,0);
+		context.g2D.setColor(myColor);
+		context.g2D.drawLine(0, context.canvas_h/2, context.canvas_w, context.canvas_h/2);
 		myColor = new Color(0,255,0);
 		context.g2D.setColor(myColor);
-		context.g2D.fillOval((i*context.canvas_w/(pointsLeft.length-1))-5, pointsLeft[i]-10, 10, 10);
 		
-		context.g2D.drawLine(i*context.canvas_w/(pointsLeft.length-1), pointsLeft[i]-5, (i+1)*context.canvas_w/(pointsLeft.length-1), pointsLeft[i+1]-5);
+
+		context.g2D.setStroke(new BasicStroke((2*context.canvas_w/Source.SAMPLES)+1));
+		for(int i = 0; i < pointsLeft.length-1; i++) {
 		
+		//context.g2D.fillOval((i*context.canvas_w/(pointsLeft.length-1))-5, pointsLeft[i]-10, 10, 10);
 		
-		myColor = new Color(0,0,255);
-		context.g2D.setColor(myColor);
-		context.g2D.fillOval((i*context.canvas_w/(pointsLeft.length-1))-5, pointsRight[i]-10, 10, 10);
+		context.g2D.drawLine(i*context.canvas_w/(pointsLeft.length-1), pointsLeft[i]/2+5, (i+1)*context.canvas_w/(pointsLeft.length-1), pointsLeft[i+1]/2+5);
 		
-		context.g2D.drawLine(i*context.canvas_w/(pointsLeft.length-1), context.canvas_h - pointsRight[i]-5, (i+1)*context.canvas_w/(pointsLeft.length-1), context.canvas_h - pointsRight[i-1]-5);
+		//context.g2D.fillOval((i*context.canvas_w/(pointsRight.length-1))-5, context.canvas_h- pointsRight[i]-10, 10, 10);
+		
+		context.g2D.drawLine(i*context.canvas_w/(pointsRight.length-1), context.canvas_h- pointsRight[i]/2-5, (i+1)*context.canvas_w/(pointsRight.length-1),context.canvas_h- pointsRight[i+1]/2-5);
+		
+		context.g2D.drawLine(i*context.canvas_w/(pointsRight.length-1), context.canvas_h/2-(pointsRight[i]-pointsLeft[i])/2, (i+1)*context.canvas_w/(pointsRight.length-1), context.canvas_h/2-(pointsRight[i+1]-pointsLeft[i+1])/2);
 		
 		}
-		myColor = new Color(0,255,0);
-		context.g2D.setColor(myColor);
 		
-		context.g2D.drawLine(context.canvas_w-5, pointsLeft[pointsLeft.length-1]-5, context.canvas_w-5, pointsLeft[pointsLeft.length-1]-5);
+//		System.out.println(Integer.toString(pointsLeft[counter]));
+//		myColor = new Color(0,255,0);
+//		context.g2D.setColor(myColor);
+//		
+//		context.g2D.drawLine(context.canvas_w-5, pointsLeft[pointsLeft.length-1]-5, context.canvas_w-5, pointsLeft[pointsLeft.length-1]-5);
+//		
+//		myColor = new Color(0,0,255);
+//		context.g2D.setColor(myColor);
+//		
+//		context.g2D.drawLine(pointsRight[pointsRight.length-1]-5, context.canvas_h-5, pointsRight[pointsRight.length-1]-5,context.canvas_h-5);
 		
-		myColor = new Color(0,0,255);
-		context.g2D.setColor(myColor);
-		
-		context.g2D.drawLine(pointsRight[pointsRight.length-1]-5, context.canvas_h-5, pointsRight[pointsRight.length-1]-5,context.canvas_h-5);
 		
 	}
 	
@@ -59,13 +72,38 @@ public class Oscilloscope extends Effect {
 			right += context.stereo_r[i];
 		}
 		
-		left = ((left - 0) * (context.canvas_h-0))/(500000 - 0);
-		right = ((right - 0) * (context.canvas_h-0))/(500000 - 0);
+		if(left > peak) {
+			peak = (int) left;
+			repeat = 0;
+		}else {
+			repeat++;
+		}
+		
+		if(right > peak) {
+			peak = (int) right;
+			repeat = 0;
+		}else {
+			repeat++;
+		}
+		
+		left = ((left - 0) * (context.canvas_h-0))/(peak - 0);
+		right = ((right - 0) * (context.canvas_h-0))/(peak - 0);
+		
+		
+		
+//		System.out.println(peak);
 		
 //		System.out.println("Vert vect: " +verticalVector + " Window Height: " + context.canvas_h + " Difference: " + (context.canvas_h-verticalVector));
 		
 		pointsLeft[counter] = (int)left;
 		pointsRight[counter] = (int)right;
+		
+		if(repeat > 100) {
+			if(peak > 100) {
+			peak -= 100;
+			}
+			repeat = 0;
+		}
 		
 		counter++;
 		if(counter >= pointsLeft.length)
