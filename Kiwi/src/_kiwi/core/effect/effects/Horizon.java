3a571d27bdb8696731d7cc3b0636db8e650b0ebb
Ghost <Ghost@Ghost.Home>
2019-09-27 21:51:15 -0600

    Added scaling for Linear!!!

--- Kiwi/src/_kiwi/core/effect/effects/Horizon.java
+++ Kiwi/src/_kiwi/core/effect/effects/Horizon.java
@@ -34,6 +34,9 @@ public class Horizon extends Effect {
 
 	//speed that the circle collapses
 	double speed;
+	
+	double peak=0;
+	int counter = 0;
 
 	//storage for position before
 	int lastTopX;
@@ -42,16 +45,16 @@ public class Horizon extends Effect {
 	int lastBotY;
 
 	BufferedImage backgroundImage; 
-//	TexturePaint back;
+	//	TexturePaint back;
 
 	//constructor
 	public Horizon() {
 		super("Horizon");
-		
+
 		//load background image
 		try {
 			backgroundImage = ImageIO.read(new File(".\\bin\\_kiwi\\core\\effect\\effects\\resources\\HorizonBackground.png"));
-//			back = new TexturePaint(backgroundImage, new Rectangle(0,0, 1920, 1080));
+			//			back = new TexturePaint(backgroundImage, new Rectangle(0,0, 1920, 1080));
 		}catch(IOException ex) {
 			System.out.println("Couldn't find file");
 		}
@@ -64,11 +67,11 @@ public class Horizon extends Effect {
 		//toggle antialising
 		context.g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		
+
 		//create a render path for the black areas of the image
 		GeneralPath top = new GeneralPath();
 		GeneralPath bottom = new GeneralPath();
-		
+
 
 		//draws background to the appropriate image and size
 		context.g2D.drawImage(backgroundImage, 0,0,context.canvas_w,context.canvas_h, 0,0, backgroundImage.getWidth(), backgroundImage.getHeight(), null);
@@ -77,25 +80,25 @@ public class Horizon extends Effect {
 		//begin drawing the shape
 		top.moveTo(points[0][0], points[1][0]);
 		bottom.moveTo(points[0][509], points[1][509]);
-		
+
 
 		//draw the rest of the points
 		for (int k = 1; k < 255; k++) {
-			top.lineTo(points[0][k], points[1][k]+context.canvas_h/2);
-			bottom.lineTo(points[0][509-k], points[1][509-k]+context.canvas_h/2);
-		
+			top.lineTo(points[0][k], points[1][k]/2+context.canvas_h/2);
+			bottom.lineTo(points[0][509-k], points[1][509-k]/2+context.canvas_h/2);
+
 		}
-		
-		
+
+
 		for(int i = 0; i < 10; i++) {
 			top.lineTo((10*context.canvas_w)/(10-i), 0);
 			bottom.lineTo((10*context.canvas_w)/(10-i), context.canvas_h);
 		}
-		
+
 		top.lineTo(0, 0);
 		bottom.lineTo(0, context.canvas_h);
-		
-		
+
+
 		//finish the shape
 		top.closePath();
 		bottom.closePath();
@@ -105,11 +108,30 @@ public class Horizon extends Effect {
 
 	public void onUpdate(UpdateContext context) {
 
+		
+		switch(context.hint) {
+
+		case LIN:
+			speed = context.canvas_h/50;
+			break;
+		case LOG:
+			speed = context.canvas_h/50;
+			scale = context.canvas_h/divider;
+			break;
+		case TANH:
+			speed = context.canvas_h/50;
+			scale = context.canvas_h/divider;
+			break;
+		}
+		
+		
 		//set speed of movement based on canvas size
-		speed = context.canvas_h/50;
+		
 
 		//set scale value based on height
-		scale = context.canvas_h/divider;
+		
+
+
 
 		//for loop to go through 1/4 of the samples
 		for(int i = 0; i < 255; i ++) {
@@ -117,8 +139,28 @@ public class Horizon extends Effect {
 			//if the value is worth checking then we find a value
 			if(context.stereo_l[i+3*Source.SAMPLES/4] > 1) {
 
-				//find the new value
-				root = scale*(Math.log(context.stereo_l[i+3*Source.SAMPLES/4]));
+				switch(context.hint) {
+
+				case LIN:
+					
+					if(context.stereo_l[i+3*Source.SAMPLES/4] > peak){
+						peak = context.stereo_l[i+3*Source.SAMPLES/4];
+						counter = 0;
+					}
+					//find the new value
+					root = ((context.stereo_l[i+3*Source.SAMPLES/4] - 0) * (context.canvas_h-0))/(peak - 0);
+					break;
+				case LOG:
+					//find the new value
+					root = scale*(Math.log(context.stereo_l[i+3*Source.SAMPLES/4]));
+					break;
+				case TANH:
+					//find the new value
+					root = scale*(Math.tanh(context.stereo_l[i+3*Source.SAMPLES/4]));	
+					break;
+				}
+
+
 
 				//if the new value is greater than the old we store it
 				if(root > peaksLeft[i]) {
@@ -146,9 +188,25 @@ public class Horizon extends Effect {
 			//if the value is worth checking
 			if(context.stereo_r[i+3*Source.SAMPLES/4] > 1) {
 
-				//find the new value
-				root = scale*(Math.log(context.stereo_r[i+3*Source.SAMPLES/4]));
+				switch(context.hint) {
 
+				case LIN:
+					if(context.stereo_r[i+3*Source.SAMPLES/4] > peak){
+						peak = context.stereo_r[i+3*Source.SAMPLES/4];
+						counter = 0;
+					}
+					//find the new value
+					root = ((context.stereo_r[i+3*Source.SAMPLES/4] - 0) * (context.canvas_h-0))/(peak - 0);
+					break;
+				case LOG:
+					//find the new value
+					root = scale*(Math.log(context.stereo_r[i+3*Source.SAMPLES/4]));
+					break;
+				case TANH:
+					//find the new value
+					root = scale*(Math.tanh(context.stereo_r[i+3*Source.SAMPLES/4]));	
+					break;
+				}
 				//if the new value is greater than the old we store it
 				if(root > peaksRight[i]) {
 					peaksRight[i] = root;
@@ -177,6 +235,13 @@ public class Horizon extends Effect {
 			points[1][i] = (int)topYValue-2;
 			points[0][509-i] = (int)botXValue;
 			points[1][509-i] = (int)botYValue+2;
+			
+			counter++;
+			
+			if(counter > 100) {
+				counter = 0;
+				peak *= .95;
+			}
 		}						
 	}
 }
